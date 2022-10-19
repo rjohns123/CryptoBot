86f53e7236748513422010cc60e63c7467d3d6dc
rjohns1 <62742190+rjohns123@users.noreply.github.com>
2022-10-19 06:40:45 -0400

    Slight overhaul
    -Algorithm is now scripts. Sounds better
    -Cleaned up code slighty
    -Scripts will now send code to either buy, sell, or nothing
    -TradeOgre API in testing
    -Now can upload API keys
    -Now can change different cryptocoins
    -No longer using CoinGecko API
    -Input will now check if input is correct

---
+++ cryptoScipts.py
@@ -0,0 +1,10 @@
+import random
+#TEMPORARY
+def startScripts(scripthmPath, moneyAmount, timeAmount):
+    randnum = random.randint(1, 3)
+    if randnum == 1:
+        return 1
+    elif randnum == 2:
+        return 2
+    elif randnum == 3:
+        return 3
--- main.py
+++ main.py
@@ -1,73 +1,136 @@
 import json
+import requests
 import os
 import configparser
-from pycoingecko import CoinGeckoAPI
+import time
+from tradeogre import TradeOgre #https://github.com/Endogen/TradeOgrePy
 from datetime import datetime
-from algorithm import *
+from cryptoScipts import *
 
-cg = CoinGeckoAPI()
-ping = json.dumps(cg.ping())
-status = json.loads(ping)
-option = 0
-while option <= 2:
-    os.system("clear")
-    print("CryptoBot 0.8\n")
-    print("1) Simulation\n2) Settings \n3) Exit")
-    option = int((input()))
-    if option == 1:
-        while option < 2:
-            os.system("clear")
-            config = configparser.ConfigParser()
-            config.read("config.ini")
-            moneyAmount = config.get("temp", "moneyAmount")
-            algorithmPath = str(config.get("temp", "algorithmPath"))
-            timeAmount= config.get("temp", "timeAmount")
-            print("Begin with these settings?\n")
-            print("Starting Amount: $" + moneyAmount + "\n")
-            print("Algorithm: " + algorithmPath + "\n")
-            print("Decision time: " + timeAmount + "m\n\n")
+config = configparser.ConfigParser()
+config.read("config.ini")
+moneyAmount = config.get("temp", "moneyAmount")
+scriptPath = str(config.get("temp", "scriptPath"))
+timeAmount = config.get("temp", "timeAmount")
+apikey = str(config.get("temp", "apikey"))
+apisecret = str(config.get("temp", "apisecret"))
+buycoin = str(config.get("temp", "buycoin"))
+sellcoin = str(config.get("temp", "sellcoin"))
+trade_ogre = TradeOgre(key=apikey, secret=apisecret)
+if (apikey and apisecret) != '':
+    isSim = False
+else:
+    isSim = True
 
-            print("1)Go\n")
-            print("2)Back\n")
-            option = int(input())
-            if option == 1:
-                os.system("clear")
-                startAlgo(algorithmPath, moneyAmount, timeAmount)
-
-    elif option == 2:
-        while option <= 3:
-            os.system("clear")
-            config = configparser.ConfigParser()
-            config.read("config.ini")
-            moneyAmount = config.get("temp", "moneyAmount")
-            algorithmPath = config.get("temp", "algorithmPath")
-            timeAmount= config.get("temp", "timeAmount")
-            print("1) Set Money(" + str(moneyAmount) + ")\n")
-            print("2) Set Alogrithm(" + str(algorithmPath) + ")\n")
-            print("3) Set Time(" + str(timeAmount) + "m)\n")
-            print("4) Go Back")
-            option = int((input()))
-            if option == 1:
-                print("Enter new amount")
-                newSetting = input()
-                config.set("temp", "moneyAmount", str(newSetting))
-                with open("config.ini", "w") as configfile:
-                    config.write(configfile)
-            elif option == 2:
-                print("Enter new path")
-                newSetting = input()
-                config.set("temp", "algorithmPath", str(newSetting))
-                with open("config.ini", "w") as configfile:
-                    config.write(configfile)
-            elif option == 3:
-                print("Enter new time")
-                newSetting = input()
-                config.set("temp", "timeAmount", str(newSetting))
-                with open("config.ini", "w") as configfile:
-                    config.write(configfile)
-        option = 0
-    elif option == 3:
+while True:
+    try:
         os.system("clear")
-        exit()
-option = 0
+        print("CryptoBot\n")
+        print("1) Simulation\n2) Settings \n3) Exit")
+        option = int((input()))
+    except ValueError:
+        continue
+    else:
+        if option == 1:
+            while True:
+                try:
+                    os.system("clear")
+                    print("Begin with these settings?\n")
+                    print("Starting Amount: $" + moneyAmount + "\n")
+                    print("Script: " + scriptPath + "\n")
+                    print("Decision Time: " + timeAmount + "m\n")
+                    print("Crypto Coins: (" + sellcoin + "/" + buycoin + ")\n")
+                    print("Simulation Mode: " + str(isSim) + "\n\n")
 
+                    print("1)Go\n")
+                    print("2)Back\n")
+                    option = int(input())
+                except ValueError:
+                    continue
+                else:
+                    if option == 1:
+                        os.system("clear")
+                        scriptDecision = 0
+                        while scriptDecision == 0:
+                            scriptDecision = startScripts(scriptPath, moneyAmount, timeAmount)
+                            if scriptDecision == 1:
+                                print("Bought")
+                                scriptDecision = 0
+                                #trade_ogre.buy(buycoin + "-" + sellcoin, 'quantity', 'price')
+                            if scriptDecision == 2:
+                                print("Sold")
+                                scriptDecision = 0
+                                #trade_ogre.sell(buycoin + "-" + sellcoin, 'quantity', 'price')
+                            if scriptDecision == 3:
+                                print("No Sale")
+                                scriptDecision = 0
+                            time.sleep(60 * int(timeAmount))
+                    if option == 2:
+                        option = 0
+                        break
+        if option == 2:
+            while True:
+                try:
+                    os.system("clear")
+                    config = configparser.ConfigParser()
+                    config.read("config.ini")
+                    moneyAmount = config.get("temp", "moneyAmount")
+                    scriptPath = config.get("temp", "scriptPath")
+                    timeAmount= config.get("temp", "timeAmount")
+                    print("1) Set Money(" + str(moneyAmount) + ")\n")
+                    print("2) Set Script(" + str(scriptPath) + ")\n")
+                    print("3) Set Time(" + str(timeAmount) + "m)\n")
+                    print("4) Set API Keys(Sim = " + str(isSim) + ")\n")
+                    print("5) Back\n\n")
+                    option = int((input()))
+                except ValueError:
+                    continue
+                else:
+                    if option == 1:
+                        while True:
+                            try:
+                                print("Enter new amount")
+                                newSetting = int(input())
+                            except ValueError:
+                                continue
+                            else:
+                                config.set("temp", "moneyAmount", str(newSetting))
+                                with open("config.ini", "w") as configfile:
+                                    config.write(configfile)
+                                break
+                    elif option == 2:
+                        print("Enter new path")
+                        newSetting = input()
+                        config.set("temp", "scriptPath", str(newSetting))
+                        with open("config.ini", "w") as configfile:
+                            config.write(configfile)
+                        continue
+                    elif option == 3:
+                        while True:
+                            try:
+                                print("Enter new time")
+                                newSetting = int(input())
+                            except ValueError:
+                                continue
+                            else:
+                                config.set("temp", "timeAmount", str(newSetting))
+                                with open("config.ini", "w") as configfile:
+                                    config.write(configfile)
+                                break
+                    elif option == 4:
+                        print("Enter your API key")
+                        apikey = input()
+                        print("Enter your Secret key")
+                        apisecret = input()
+                        config.set("temp", "apikey", str(apikey))
+                        config.set("temp", "apisecret", str(apisecret))
+                        with open("config.ini", "w") as configfile:
+                            config.write(configfile)
+                        continue
+                    elif option == 5:
+                        option = 0
+                        break
+        if option == 3:
+            break
+os.system("clear")
+exit()
